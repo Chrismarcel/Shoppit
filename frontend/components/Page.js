b1e56379c0127278d3cd2c225742f37130ca08e9
Chrismarcel <cmarcelj@gmail.com>
2019-10-25 08:42:37 +0100

    add meta tags

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Meta from './Meta';
 
 class Page extends Component {
     render() {
         return (
             <div>
+                <Meta /> 
                 Hey I'm the page component
                 {this.props.children}
             </div>
