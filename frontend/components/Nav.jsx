0f47862efb8113a412ccacf07dbf463c7ba42326
Chrismarcel <cmarcelj@gmail.com>
2019-10-26 18:00:02 +0100

    add and style navigation links

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -1,5 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
 const Nav = () => (
-  <p>Hello</p>
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/sell">
+      <a>Signup</a>
+    </Link>
+    <Link href="/sell">
+      <a>Orders</a>
+    </Link>
+    <Link href="/sell">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 );
 
 export default Nav;
